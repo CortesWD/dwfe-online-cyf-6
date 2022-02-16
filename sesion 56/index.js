5ce14c9eb0bac78324e79dec3827f6253d17b11a
CortesWD <cccp.designer@gmail.com>
2022-02-15 19:36:20 -0500

    fns documentadas

--- sesion 56/index.js
+++ sesion 56/index.js
@@ -1,5 +1,14 @@
+/**
+ * @description console log fn wrapper
+ * @param {*} msn 
+ */
 const logger = (msn) => console.log(msn);
 
+/**
+ * @description sum two intg
+ * @param {Number} num1 
+ * @param {Number} num2
+ */
 const add = (num1,num2) => logger(num1 + num2);
 
 add(1,2);
