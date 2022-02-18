ab4251565eaaac92bb33038cd76414657c37313e
CortesWD <cccp.designer@gmail.com>
2022-02-17 20:25:17 -0500

    [57] - añadida fn de potencia

--- sesion 57/index.js
+++ sesion 57/index.js
@@ -20,3 +20,11 @@ const getGreater = (a,b) => {
 }
 
 getGreater(200000,2000001);
+
+const potenciaDeDos = (n) => {
+  const calc = Math.pow(n, 2);
+  logger(`potencia dos de ${n} es ${calc}`);
+  return calc;
+}
+
+potenciaDeDos(3);
