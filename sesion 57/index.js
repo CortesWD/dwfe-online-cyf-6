ec61d8b0ffe3b453d443230db7ec4c7d703602ed
CortesWD <cccp.designer@gmail.com>
2022-02-17 20:46:19 -0500

    added doc

--- sesion 57/index.js
+++ sesion 57/index.js
@@ -21,6 +21,10 @@ const getGreater = (a,b) => {
 
 getGreater(200000,2000001);
 
+/**
+ * @description potenciar numero
+ * @param {*} msn 
+ */
 const potenciaDeDos = (n) => {
   const calc = Math.pow(n, 2);
   logger(`potencia dos de ${n} es ${calc}`);
